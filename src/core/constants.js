3cf9154f177ebb5bfc4db8b176ab118b2ef33b19
Supratik Sarkar <87625070+supratiksarkaremp22@users.noreply.github.com>
2022-07-13 16:34:41 +0530

    Create constants.js

---
+++ src/core/constants.js
@@ -0,0 +1,4 @@
+export const addressDeleteURL = id => `${endpoint}/addresses/${id}/delete/`;
+export const orderItemDeleteURL = id => `${endpoint}/order-items/${id}/delete/`;
+export const orderItemUpdateQuantityURL = `${endpoint}/order-item/update-quantity/`;
+export const paymentListURL = `${endpoint}/payments/`;
